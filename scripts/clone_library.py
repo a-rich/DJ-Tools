baf4ef03626706b8d2a6ff4570bf8d144e9ef4f7
Alex Richards <alex.richards006@gmail.com>
2021-04-27 18:50:36 -0700

    debug windows shit

--- scripts/clone_library.py
+++ scripts/clone_library.py
@@ -51,27 +51,28 @@ if __name__ == '__main__':
             cmd = f"aws s3 cp s3://dj.beatcloud.com/dj/xml/rekordbox.xml '{os.path.join(args.path, 'PIONEER', 'rekordbox.xml')}'"
             os.system(cmd)
 
-            if os.name == 'posix':
-                print(f"Rewritting rekordbox.xml track locations for {args.path}...")
-                """ # TODO: write XML with same whitespace formatting so Rekordbox can read it
-                data = xmltodict.parse(open(os.path.join(args.path, 'PIONEER', 'rekordbox.xml'), 'r').read())
-                for track in data['DJ_PLAYLISTS']['COLLECTION']['TRACK'][-1::-1]:
-                    track['@Location'] = track['@Location'].replace('/Volumes/DJ/', os.path.join(args.path if os.name == 'posix' else os.path.splitdrive(args.path)[0], ''))
-                json.dump(data, open(os.path.join(args.path, 'PIONEER', 'rekordbox.xml'), 'w'))
-                """
-                lines = open(os.path.join(args.path, 'PIONEER', 'rekordbox.xml'), 'r').readlines()
-                with open(os.path.join(args.path, 'PIONEER', 'rekordbox.xml'), 'w') as f:
-                    for line in lines:
-                        if 'file://localhost' in line:
-                            line = line.replace('/Volumes/DJ/', os.path.join(args.path if os.name == 'posix' else os.path.splitdrive(args.path)[0], ''))
-                        f.write(f"{line.strip()}\n")
-            else:
-                print(f"Rewritting rekordbox.xml track locations for your system ({os.name}) is not yet supported")
-                # example rekordbox.xml location formatted on Windows 10
-                # Location="file://localhost/D:/DJ%20Music/Bass/Architek%20%26%20Visceral%20-%20Broken.mp3"
+            # if os.name == 'posix':
+            #     print(f"Rewritting rekordbox.xml track locations for {args.path}...")
+            #     """ # TODO: write XML with same whitespace formatting so Rekordbox can read it
+            #     data = xmltodict.parse(open(os.path.join(args.path, 'PIONEER', 'rekordbox.xml'), 'r').read())
+            #     for track in data['DJ_PLAYLISTS']['COLLECTION']['TRACK'][-1::-1]:
+            #         track['@Location'] = track['@Location'].replace('/Volumes/DJ/', os.path.join(args.path if os.name == 'posix' else os.path.splitdrive(args.path)[0], ''))
+            #     json.dump(data, open(os.path.join(args.path, 'PIONEER', 'rekordbox.xml'), 'w'))
+            #     """
+            print(f"os.name: {os.name}\nsplitting {args.path}: {os.path.splitdrive(args.path)}")
+            lines = open(os.path.join(args.path, 'PIONEER', 'rekordbox.xml'), 'r').readlines()
+            with open(os.path.join(args.path, 'PIONEER', 'rekordbox.xml'), 'w') as f:
+                for line in lines:
+                    if 'file://localhost' in line:
+                        line = line.replace('/Volumes/DJ/', os.path.join(args.path if os.name == 'posix' else os.path.splitdrive(args.path)[0], ''))
+                    f.write(f"{line.strip()}\n")
+            # else:
+            #     print(f"Rewritting rekordbox.xml track locations for your system ({os.name}) is not yet supported")
+            #     # example rekordbox.xml location formatted on Windows 10
+            #     # Location="file://localhost/D:/DJ%20Music/Bass/Architek%20%26%20Visceral%20-%20Broken.mp3"
 
-                # example rekordbox.xml location formatted on Mac
-                # Location="file://localhost/Volumes/DJ_BACKUP/DJ%20Music/Bass/Architek%20%26%20Visceral%20-%20Broken.mp3"
+            #     # example rekordbox.xml location formatted on Mac
+            #     # Location="file://localhost/Volumes/DJ_BACKUP/DJ%20Music/Bass/Architek%20%26%20Visceral%20-%20Broken.mp3"
 
     if args.upload:
         hidden = set(glob(f"{os.path.join(args.path, 'DJ Music', '**', '.*.*')}", recursive=True))
