c54b5b8ed52ae93313e8b4143d49754e33954c46
Alex Richards <alex.richards006@gmail.com>
2022-04-03 13:15:04 -0700

    praw cache

--- src/djtools/spotify/playlist_checker.py
+++ src/djtools/spotify/playlist_checker.py
@@ -64,7 +64,7 @@ def get_spotify_tracks(config):
                 redirect_uri=config['SPOTIFY_REDIRECT_URI'],
                 scope='playlist-modify-public',
                 cache_path=os.path.join(os.path.dirname(__file__),
-                                        '.cache').replace(os.sep, '/')))
+                        '.spotify.cache').replace(os.sep, '/')))
     except KeyError:
         raise KeyError('Using the playlist_checker module requires the ' \
                        'following config options: SPOTIFY_CLIENT_ID, ' \
