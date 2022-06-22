ebb83bfe07053f0afca6916218d6b5961d5dadd9
Alex Richards <alex.richards006@gmail.com>
2022-06-22 10:54:22 -0700

    2.2.0-beta12

--- setup.py
+++ setup.py
@@ -71,7 +71,7 @@ EXTRAS = {
 
 setup(
     name='dj_beatcloud',
-    version='2.2.0-beta11',
+    version='2.2.0-beta12',
     description='DJ Tools is a library for managing a collection of MP3 ' \
                 'and Rekordbox XML files.',
     long_description=LONG_DESCRIPTION,
