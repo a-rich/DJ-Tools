d95c35fe1cf4f04764522091b3c04246d3922440
Alex Richards <4131505+a-rich@users.noreply.github.com>
2023-08-29 17:58:03 -0700

    chore: test dev workflows (#152)

--- djtools/__init__.py
+++ djtools/__init__.py
@@ -66,6 +66,7 @@ __all__ = (
 def main():
     """This is the entry point for the DJ Tools library."""
 
+    # Testing release_dev workflow
     logger, log_file = initialize_logger()
     config = build_config()
     logger.setLevel(config.LOG_LEVEL)
