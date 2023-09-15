0da4c22c23efa6477aafaaa67dc70effbbcf9ec1
Alex Richards <4131505+a-rich@users.noreply.github.com>
2023-09-15 13:56:06 -0700

    test: missing coverage on reduced BaseConfig repr (#177)
    Why?
    Coverage.
    
    What?
    Mock the frame stack to simulate calling djtools as a CLI.

--- testing/configs/test_config.py
+++ testing/configs/test_config.py
@@ -1,7 +1,22 @@
 """Testing for the config module."""
+from unittest import mock
+
 from djtools.configs.config import BaseConfig
 
 
 def test_baseconfig_aws_profile():
     """Test for the BaseConfig class."""
     BaseConfig()
+
+
+@mock.patch(
+    "inspect.stack",
+    new=mock.Mock(
+        return_value=[
+            "", "", ["", "", "", "build_config"], ["", "main/bin/djtools"]
+        ],
+    )
+)
+def test_baseconfig_limits_repr_in_cli_execution():
+    """Test for the BaseConfig class."""
+    BaseConfig(key="value")
