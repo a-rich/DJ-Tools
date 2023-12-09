4c7b71c055009f2be51bc4b05c30525d2e1e2498
Alex Richards <alex.richards006@gmail.com>
2023-12-09 08:16:03 -0800

    script: clustering and sorting vibe tags

---
+++ scripts/collection/cluster_tracks.py
@@ -0,0 +1,170 @@
+from argparse import ArgumentParser
+from pathlib import Path
+from typing import Dict, List, Tuple, Optional
+
+import numpy as np
+import pandas as pd
+from sklearn.cluster import DBSCAN, KMeans, SpectralClustering
+from tqdm import tqdm
+
+from djtools.collection.collections import Collection
+from djtools.collection.playlists import Playlist
+from djtools.collection.helpers import PLATFORM_REGISTRY
+from djtools.collection.tracks import Track
+from djtools.configs import build_config
+
+
+EXCLUDE_TAGS = (
+    "DELETE",
+    "Flute",
+    "Guitar",
+    "Horn",
+    "Piano",
+    "Scratch",
+    "Strings",
+    "Vocal",
+)
+PLAYLIST_NAME = "Clusters"
+
+
+def get_collection_and_playlist_class(
+    config_path: Path, collection_path: Path
+) -> Tuple[Collection, Playlist]:
+    config = build_config(config_path)
+    if args.collection:
+        config.COLLECTION_PATH = collection_path
+
+    collection = PLATFORM_REGISTRY[config.PLATFORM]["collection"](
+        path=config.COLLECTION_PATH
+    )
+    playlist_class = PLATFORM_REGISTRY[config.PLATFORM]["playlist"]
+
+    return collection, playlist_class
+
+
+def get_tracks(
+    collection: Collection, playlists: Optional[List[str]] = None
+) -> Dict[str, Track]:
+    if not playlists:
+        tracks = collection.get_tracks()
+    else:
+        tracks = {}
+        for playlist_name in playlists:
+            for playlist in collection.get_playlists(playlist_name):
+                tracks.update(playlist.get_tracks())
+
+    return tracks
+
+
+def dataprep(collection: Collection, tracks: Dict[str, Track]) -> pd.DataFrame:
+    other_tags = sorted(
+        set(collection.get_all_tags()["other"]).difference(EXCLUDE_TAGS)
+    )
+    dataset = []
+    for track in tqdm(tracks.values(), desc="Building dataset from tracks"):
+        track_id = track.get_id()
+        tags = set(track.get_tags()).difference(set(track.get_genre_tags()))
+        one_hot = [0] * len(other_tags)
+        for index, tag in enumerate(other_tags):
+            if tag not in tags:
+                continue
+            one_hot[index] = 1
+        dataset.append((track_id, *one_hot))
+    df = pd.DataFrame(dataset, columns=["id"] + other_tags, index=None)
+    df.set_index("id", inplace=True)
+
+    return df
+
+
+def cluster(
+    df: pd.DataFrame,
+    clusters: List[int],
+    playlist_class: Playlist,
+    tracks: Dict[str, Track],
+    cluster_algo_name: str,
+) -> List[Playlist]:
+    playlists = []
+    cluster_algos = {
+        "kmeans": KMeans,
+        "dbscan": DBSCAN,
+        "spectral": SpectralClustering,
+    }
+
+    for num_clusters in clusters:
+        if cluster_algo_name in ["kmeans", "spectral"]:
+            kwargs = {"n_clusters": num_clusters, "n_init": 10}
+        else:
+            kwargs = {"eps": num_clusters, "min_samples": 10}
+        cluster_playlists = []
+        cluster_algo = cluster_algos[cluster_algo_name](**kwargs).fit(df)
+        if getattr(cluster_algo, "inertia_", None):
+            print(
+                f"{num_clusters} clusters -- inertia: {cluster_algo.inertia_}:"
+            )
+        else:
+            print(f"{num_clusters} clusters")
+        for cluster in range(num_clusters):
+            indices = np.where(cluster_algo.labels_ == cluster)
+            track_ids = df.iloc[indices].index.tolist()
+            print(f"\t{len(track_ids)}")
+            cluster_playlists.append(
+                playlist_class.new_playlist(
+                    name=str(cluster + 1),
+                    tracks={
+                        track_id: tracks[track_id] for track_id in track_ids
+                    },
+                )
+            )
+        playlists.append(
+            playlist_class.new_playlist(
+                name=f"{num_clusters} clusters", playlists=cluster_playlists
+            )
+        )
+
+    return playlists
+
+
+def save_playlists(collection: Collection, playlists: List[Playlist]):
+    previous_playlists = collection.get_playlists(name=PLAYLIST_NAME)
+    root = collection.get_playlists()
+    for playlist in previous_playlists:
+        root.remove_playlist(playlist)
+    collection.add_playlist(
+        playlist_class.new_playlist(name=PLAYLIST_NAME, playlists=playlists)
+    )
+    collection.serialize()
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument(
+        "--cluster_algo_name",
+        type=str,
+        default="kmeans",
+        help="Clustering algorithm to use.",
+    )
+    parser.add_argument(
+        "--clusters",
+        type=int,
+        nargs="+",
+        default=[5],
+        help="Number of clusters.",
+    )
+    parser.add_argument(
+        "--collection", required=False, help="Path to collection."
+    )
+    parser.add_argument("--config", required=True, help="Path to config.yaml.")
+    parser.add_argument(
+        "--playlists", nargs="+", help="Playlist names to cluster."
+    )
+    args = parser.parse_args()
+
+    collection, playlist_class = get_collection_and_playlist_class(
+        Path(args.config), Path(args.collection)
+    )
+    tracks = get_tracks(collection, args.playlists)
+    dataset = dataprep(collection, tracks)
+    playlists = cluster(
+        dataset, args.clusters, playlist_class, tracks, args.cluster_algo_name
+    )
+    save_playlists(collection, playlists)
--- scripts/collection/sort_tags.py
+++ scripts/collection/sort_tags.py
@@ -3,6 +3,7 @@ alphabetical order.
 """
 from argparse import ArgumentParser
 from pathlib import Path
+import re
 
 from djtools.configs import build_config
 from djtools.collection.helpers import PLATFORM_REGISTRY
@@ -27,6 +28,7 @@ TAG_ORDERINGS = (
     ),
     ("Flute", "Guitar", "Horn", "Piano", "Strings", "Vocal"),
     ("Loop", "Scratch"),
+    ("DELETE",),
 )
 
 
@@ -49,12 +51,14 @@ if __name__ == "__main__":
     collection = PLATFORM_REGISTRY[config.PLATFORM]["collection"](
         path=config.COLLECTION_PATH
     )
+    playlist_class = PLATFORM_REGISTRY[config.PLATFORM]["playlist"]
 
     tracks = collection.get_tracks()
     tag_sets = [set(tags) for tags in TAG_ORDERINGS]
-    affected = not_affected = 0
+    my_tags_regex = r"(?<=\/\*).*(?=\*\/)"
+    affected_tracks = {}
 
-    for track in tracks.values():
+    for track_id, track in tracks.items():
         all_tags = track.get_tags()
         genre_tags = set(track.get_genre_tags())
         track_tag_set = set(all_tags).difference(genre_tags)
@@ -63,15 +67,24 @@ if __name__ == "__main__":
             new_tags.extend(
                 sorted([tag for tag in track_tag_set.intersection(tag_set)])
             )
+
         tags_before = [tag for tag in all_tags if tag not in genre_tags]
         if tags_before != new_tags:
-            affected += 1
-            print(f"{tags_before}\n{new_tags}\n")
-            # track._Tags = new_tags
-        else:
-            not_affected += 1
+            print(
+                f"{track._Name} - {track._Artist}\n{tags_before}\n{new_tags}\n"
+            )
+            track._Comments = re.sub(
+                my_tags_regex,
+                " " + " / ".join(new_tags) + " ",
+                track._Comments,
+            )
+            affected_tracks[track_id] = track
 
-    # collection.serialize()
+    collection.add_playlist(
+        playlist_class.new_playlist(
+            name="Tracks with sorted comments", tracks=affected_tracks
+        )
+    )
+    collection.serialize()
 
-    print(f"affected: {affected}")
-    print(f"not affected: {not_affected}")
+    print(f"{len(affected_tracks)} out of {len(tracks)} tracks were affected.")
