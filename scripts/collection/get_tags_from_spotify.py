b318cd7e89a7a9f77b0bf9c7c1545e0cf71acd99
Alex Richards <4131505+a-rich@users.noreply.github.com>
2023-12-31 16:07:34 -0800

    Add tags (#255)
    script: interactive mode get_tags_from_spotify

--- scripts/collection/get_tags_from_spotify.py
+++ scripts/collection/get_tags_from_spotify.py
@@ -8,7 +8,6 @@ from argparse import ArgumentParser
 from concurrent.futures import as_completed, ThreadPoolExecutor
 from datetime import datetime
 import os
-from pathlib import Path
 
 from tqdm import tqdm
 
@@ -17,7 +16,7 @@ from djtools.collection.helpers import PLATFORM_REGISTRY
 from djtools.spotify.helpers import filter_results, get_spotify_client
 
 
-def get_spotify_tags_thread(track, spotify, threshold):
+def get_spotify_tags_thread(track, spotify, threshold, query_limit):
     """Threaded function.
 
     Hits the Spotify API and tries to find a matching track. If found, resolve
@@ -27,11 +26,12 @@ def get_spotify_tags_thread(track, spotify, threshold):
         track: A Track object.
         spotify: Spotify client.
         threshold: Minimum Spotify result similarity for a match.
+        query_limit: Number of Spotify query results.
     """
     title = getattr(track, "_Name")
     artist = track.get_artists()
     results = spotify.search(
-        q=f"track:{title} artist:{artist}", type="track", limit=50
+        q=f"track:{title} artist:{artist}", type="track", limit=query_limit
     )
     result, _ = filter_results(spotify, results, threshold, title, artist)
     if result:
@@ -51,38 +51,130 @@ def get_spotify_tags_thread(track, spotify, threshold):
 
 if __name__ == "__main__":
     arg_parser = ArgumentParser()
-    arg_parser.add_argument("--config", help="Path to a config.yaml")
+    arg_parser.add_argument("--collection", help="Path to a collection.")
+    arg_parser.add_argument("--config", help="Path to a config.yaml.")
     arg_parser.add_argument(
-        "--output_collection", help="Path to output collection"
+        "--mode",
+        choices=["bulk", "interactive"],
+        default="interactive",
+        help="Bulk process tracks or review individual tracks.",
+    )
+    arg_parser.add_argument("--output", help="Path to output collection.")
+    arg_parser.add_argument(
+        "--query-limit",
+        type=int,
+        default=50,
+        help="Number of Spotify query results.",
+    )
+    arg_parser.add_argument(
+        "--similarity",
+        type=int,
+        default=90,
+        help="Minimum Levenshtein distance for a Spotify query result to match a track.",
     )
     args = arg_parser.parse_args()
 
     # Build config, instantiate collection, and get tracks.
-    config_path = Path(args.config)
-    config = build_config(config_path)
+    config = build_config(args.config)
     collection = PLATFORM_REGISTRY[config.PLATFORM]["collection"](
-        path=config.COLLECTION_PATH
+        path=args.collection or config.COLLECTION_PATH
     )
-    tracks = collection.get_tracks().values()
+    tracks = collection.get_tracks()
+    spotify = get_spotify_client(config)
+    playlist_class = PLATFORM_REGISTRY[config.PLATFORM]["playlist"]
 
     # Add tags from Spotify.
-    spotify = get_spotify_client(config)
-    threshold = config.SPOTIFY_PLAYLIST_FUZZ_RATIO
-    with tqdm(
-        total=len(tracks), desc="Adding tags from Spotify"
-    ) as pbar, ThreadPoolExecutor(max_workers=os.cpu_count() * 4) as pool:
-        futures = [
-            pool.submit(get_spotify_tags_thread, track, spotify, threshold)
-            for track in tracks
-        ]
-        for future in as_completed(futures):
-            future.result()
-            pbar.update(1)
+    if args.mode == "bulk":
+        with tqdm(
+            total=len(tracks), desc="Adding tags from Spotify"
+        ) as pbar, ThreadPoolExecutor(max_workers=os.cpu_count() * 4) as pool:
+            futures = [
+                pool.submit(
+                    get_spotify_tags_thread, track, spotify, args.similarity
+                )
+                for track in tracks
+            ]
+            for future in as_completed(futures):
+                future.result()
+                pbar.update(1)
 
-    playlist_class = PLATFORM_REGISTRY[config.PLATFORM]["playlist"]
-    playlist = playlist_class.new_playlist(
-        name=args.playlist_name, tracks=tracks
-    )
-    collection.add_playlist(playlist)
-    output = args.output_collection if args.output_collection else config_path
-    collection.serialize(output)
+        playlist = playlist_class.new_playlist(
+            name=args.playlist_name, tracks=tracks
+        )
+        collection.add_playlist(playlist)
+    elif args.mode == "interactive":
+        no_matches = {}
+        same_tracks = {}
+        updated_tracks = {}
+
+        for track_id, track in tqdm(tracks.items()):
+            # Query Spotify for matching tracks
+            title = track._Name
+            artist = track.get_artists()
+            query = f"track:{title} artist:{artist}"
+            results = spotify.search(
+                q=query, type="track", limit=args.query_limit
+            )
+
+            # Get result most similar to query.
+            result, _ = filter_results(
+                spotify, results, args.similarity, title, artist
+            )
+
+            # Mark track as a non-match and continue.
+            if not result:
+                no_matches[track_id] = track
+                continue
+
+            # Query album for the track.
+            album = spotify.album(result["album"]["id"])
+            for date_format in ["%Y-%m-%d", "%Y-%m", "%Y"]:
+                try:
+                    date = datetime.strptime(
+                        album["release_date"], date_format
+                    )
+                except ValueError:
+                    continue
+
+            # Display comparison of track data from Collection vs. Spotify.
+            artists = ", ".join([x["name"] for x in result["artists"]])
+            print(
+                f"C: {title} - {artist}: "
+                f"{track._Album}, {track._Label}, {track._Year}\n"
+                f"S: {result['name']} - {artists}: "
+                f"{album['name']}, {album['label']}, {str(date.year)}"
+            )
+
+            # Check input for signal to skip or accept new tags.
+            next = False
+            while not next:
+                resp = input("[y] accept, [n] skip")
+                if "n" in resp:
+                    same_tracks[track_id] = track
+                    next = True
+                elif "y" in resp:
+                    for attribute_name, attribute in [
+                        ("album", album["name"]),
+                        ("label", album["label"]),
+                        ("year", str(date.year)),
+                    ]:
+                        setattr(track, f"_{attribute_name.title()}", attribute)
+                        updated_tracks[track_id] = track
+                    next = True
+                else:
+                    print(
+                        "Response must contain either 'n' or 'y' -- try again!"
+                    )
+
+        playlist_class = PLATFORM_REGISTRY[config.PLATFORM]["playlist"]
+        for name, set_tracks in [
+            ("No matches", no_matches),
+            ("Same", same_tracks),
+            ("Updated", updated_tracks),
+        ]:
+            playlist = playlist_class.new_playlist(
+                name=name, tracks=set_tracks
+            )
+            collection.add_playlist(playlist)
+
+    collection.serialize(path=args.output or config.COLLECTION_PATH)
