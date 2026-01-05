07e4730646cd3fdbc588c63e7c59967021a214dc
Alex Richards <alrichards@nvidia.com>
2026-01-05 19:25:38 +0000

    Trivial change to trigger 2.9.1-rc2 build

--- src/djtools/version.py
+++ src/djtools/version.py
@@ -13,11 +13,9 @@ def get_version() -> str:
         Version string.
     """
     version = importlib.metadata.version(__package__)
-
-    # For some reason, importlib doesn't include dashes which are necessary for
-    # semver.Version.parse.
     major_minor_patch_regex = re.compile(r"([0-9]+)\.([0-9]+)\.([0-9]+)")
     match = re.match(major_minor_patch_regex, version).group()
+
     if match:
         suffix = version.split(match)[-1]
         if suffix and not suffix.startswith("-"):
